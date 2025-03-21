5f51ab71d94f9d3fb0f37016bafe2aa8955819ef
Luis Marcato <luis.marcato2000@gmail.com>
2025-03-20 21:28:50 -0300

    feat: created folder devcontainer

--- api/main.py
+++ api/main.py
@@ -1,14 +1,27 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class InfoPrevisao(BaseModel):
+    empresa: str
+    volume: float
+    prev_fechamento: float
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def principal():
+    return {"status" : True, 
+            "message": "Hello World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/previsoes")
+def previsoes(infoPrevisao: InfoPrevisao):
+    w0, w1, w2 = [-15.3, 1.0, -3.2]
+
+    if (infoPrevisao.empresa == 'aapl'):
+        previsao = w0 + w1 * infoPrevisao.prev_fechamento + w2 * infoPrevisao.volume
+    else :
+        previsao = 0
+
+    return {"profecia": previsao}
